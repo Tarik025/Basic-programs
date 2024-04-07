9e24997a6796adb8c631c56a6c5e62a9e6ccb87c
Tarik025 <136357577+Tarik025@users.noreply.github.com>
2024-04-07 14:18:51 +0530

    Add files via upload

---
+++ PNum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PNum{
+
+public static void main(String [] args){
+	
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Enter a number, upto which you want to print numbers");
+	int n =sc.nextInt();
+
+	for(int i=0; i<= n; i++){
+		System.out.println(i);
+
+
+  }
+ }
+}
+                                             
